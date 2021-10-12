d81b43d9e082fa3fb3ab2d0e03a39eedd02beb99
Niels Langlotz <53429393+typoniels@users.noreply.github.com>
2021-10-13 00:26:59 +0200

    Update CollectionInfo.php

--- Classes/Domain/Transfer/CollectionInfo.php
+++ Classes/Domain/Transfer/CollectionInfo.php
@@ -1,141 +1,221 @@
 <?php
 
-declare(strict_types=1);
+namespace TN\Typoniels\Domain\Model;
 
-/*
- * This file is part of the "Image Gallery" extension for TYPO3 CMS.
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
+/**
+ * Class Question
  *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
+ * @package TN\Typoniels\Domain\Model
  */
-
-namespace Leuchtfeuer\BmImageGallery\Domain\Transfer;
-
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Resource\Collection\AbstractFileCollection;
-use TYPO3\CMS\Core\Resource\FileInterface;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
-class CollectionInfo
+class Repository extends AbstractEntity
 {
-    protected $identifier = 0;
-
-    protected $title = '';
-
-    protected $description = '';
-
-    protected $itemCount = 0;
-
-    protected $preview;
+    /**
+     * @var int
+     */
+    protected $reproId = 0;
 
     /**
-     * @var \DateTimeInterface|null
+     * title
+     *
+     * @var string
      */
-    protected $date;
+    protected $reproName = '';
 
-    protected $location = '';
+    /**
+     * title
+     *
+     * @var string
+     */
+    protected $reproDescription = '';
 
-    public function __construct(AbstractFileCollection $fileCollection, array $fileObjects)
+    /**
+     * title
+     *
+     * @var string
+     */
+    protected $reproWebUrl = '';
+
+    /**
+     * title
+     *
+     * @var string
+     */
+    protected $reproTagList = '';
+
+    /**
+     * title
+     *
+     * @var string
+     */
+    protected $reproAvatarUrl = '';
+
+    /**
+     * title
+     *
+     * @var string
+     */
+    protected $reproLastActivityAt = '';
+
+    /**
+     * title
+     *
+     * @var string
+     */
+    protected $description = '';
+
+
+    /**
+     * media
+     *
+     * @var \TN\Typoniels\Domain\Model\FileReference|null
+     */
+    protected $media = null;
+
+    /**
+     * @return int
+     */
+    public function getReproId(): int
     {
-        $this->setIdentifier($fileCollection->getUid());
-        $this->setTitle((string)$fileCollection->getTitle());
-        $this->setItemCount(count($fileObjects));
-        $this->setPreview(reset($fileObjects));
-        $this->loadGalleryData();
-
-        if (empty($this->description)) {
-            $this->setDescription((string)$fileCollection->getDescription());
-        }
+        return $this->reproId;
     }
 
-    public function getIdentifier(): int
+    /**
+     * @param int $reproId
+     */
+    public function setReproId(int $reproId): void
     {
-        return $this->identifier;
+        $this->reproId = $reproId;
     }
 
-    public function setIdentifier(int $identifier): void
+    /**
+     * @return string
+     */
+    public function getReproName(): string
     {
-        $this->identifier = $identifier;
+        return $this->reproName;
     }
 
-    public function getTitle(): string
+    /**
+     * @param string $reproName
+     */
+    public function setReproName(string $reproName): void
     {
-        return $this->title;
+        $this->reproName = $reproName;
     }
 
-    public function setTitle(string $title): void
+    /**
+     * @return string
+     */
+    public function getReproDescription(): string
     {
-        $this->title = $title;
+        return $this->reproDescription;
     }
 
+    /**
+     * @param string $reproDescription
+     */
+    public function setReproDescription(string $reproDescription): void
+    {
+        $this->reproDescription = $reproDescription;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReproWebUrl(): string
+    {
+        return $this->reproWebUrl;
+    }
+
+    /**
+     * @param string $reproWebUrl
+     */
+    public function setReproWebUrl(string $reproWebUrl): void
+    {
+        $this->reproWebUrl = $reproWebUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReproTagList(): string
+    {
+        return $this->reproTagList;
+    }
+
+    /**
+     * @param string $reproTagList
+     */
+    public function setReproTagList(string $reproTagList): void
+    {
+        $this->reproTagList = $reproTagList;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReproAvatarUrl(): string
+    {
+        return $this->reproAvatarUrl;
+    }
+
+    /**
+     * @param string $reproAvatarUrl
+     */
+    public function setReproAvatarUrl(string $reproAvatarUrl): void
+    {
+        $this->reproAvatarUrl = $reproAvatarUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReproLastActivityAt(): string
+    {
+        return $this->reproLastActivityAt;
+    }
+
+    /**
+     * @param string $reproLastActivityAt
+     */
+    public function setReproLastActivityAt(string $reproLastActivityAt): void
+    {
+        $this->reproLastActivityAt = $reproLastActivityAt;
+    }
+
+    /**
+     * @return string
+     */
     public function getDescription(): string
     {
         return $this->description;
     }
 
+    /**
+     * @param string $description
+     */
     public function setDescription(string $description): void
     {
         $this->description = $description;
     }
 
-    public function getItemCount(): int
+    /**
+     * @return null|FileReference
+     */
+    public function getMedia(): ?FileReference
     {
-        return $this->itemCount;
+        return $this->media;
     }
 
-    public function setItemCount(int $itemCount): void
+    /**
+     * @param null|FileReference $media
+     */
+    public function setMedia(?FileReference $media): void
     {
-        $this->itemCount = $itemCount;
+        $this->media = $media;
     }
 
-    public function getPreview(): FileInterface
-    {
-        return $this->preview;
-    }
-
-    public function setPreview(FileInterface $preview): void
-    {
-        $this->preview = $preview;
-    }
-
-    public function getDate(): ?\DateTimeInterface
-    {
-        return $this->date;
-    }
-
-    public function setDate(\DateTimeInterface $date): void
-    {
-        $this->date = $date;
-    }
-
-    public function getLocation(): string
-    {
-        return $this->location;
-    }
-
-    public function setLocation(string $location): void
-    {
-        $this->location = $location;
-    }
-
-    protected function loadGalleryData()
-    {
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_collection');
-
-        $properties = $queryBuilder
-            ->select('bm_image_gallery_description', 'bm_image_gallery_location', 'bm_image_gallery_date')
-            ->from('sys_file_collection')
-            ->where($queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($this->identifier, \PDO::PARAM_INT)))
-            ->execute()
-            ->fetch();
-
-        $this->setDescription($properties['bm_image_gallery_description']);
-        $this->setLocation($properties['bm_image_gallery_location']);
-
-        if ($properties['bm_image_gallery_date'] > 0) {
-            $this->setDate((new \DateTime())->setTimestamp($properties['bm_image_gallery_date']));
-        }
-    }
 }
